932660bfdb7a649d9bc3c993ed9547a665983ff0
sachinmukherjee <sachinmukherjee29@gmail.com>
2017-10-22 16:00:05 +0530

    balancing of symbol implemented

---
+++ Stack/Balancing of Symbols/BalancingOfSymbols.java
@@ -0,0 +1,72 @@
+import java.io.*;
+class BalancingOfSymbols
+{
+	char array[];
+	int top=-1;
+	
+	BalancingOfSymbols(int size)
+	{
+		array = new char[size];
+	}
+	
+	void push(char symbol)
+	{
+		array[++top] = symbol;
+	}
+	
+	char pop()
+	{
+		return array[top--];
+	}
+	
+	boolean isEmpty()
+	{
+		return top ==-1;
+	}
+	
+	public static void main(String args[]) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		int len = input.length();
+		BalancingOfSymbols balance = new BalancingOfSymbols(len);
+		int i=0;
+		while(i<len)
+		{
+			char a = input.charAt(i);
+			
+			if((a!='[' && a!='(' && a!='{') && (a!='}' && a!=']' && a!=')'))
+				i++;
+			else if(a=='{' || a=='[' || a=='(')
+			{
+				balance.push(a);
+				i++;
+			}
+			else
+			{
+				if(balance.isEmpty())
+				{
+					System.out.println("Error Mismatching");
+					break;
+				}
+				else
+				{
+					char check = balance.pop();
+					if((a=='['&& check!=']') || (a =='{' && check != '}') || (a == '(' && check != ')'))
+					{
+						System.out.println("Error Mismatching!");
+						break;
+					}
+					else 
+					{
+						i++;
+					}
+				}
+			}
+			
+		}
+		if(!balance.isEmpty())
+			System.out.println("Error Reporting!!");
+		
+	}
+}
